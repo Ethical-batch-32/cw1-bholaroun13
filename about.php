e879391020a28489d6819e8e5ca6abdee1f194a0
bholaroun13 <108653015+bholaroun13@users.noreply.github.com>
2023-01-18 12:33:44 +0545

    Add files via upload

---
+++ about.php
@@ -0,0 +1,252 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+   <meta charset="UTF-8">
+   <meta http-equiv="X-UA-Compatible" content="IE=edge">
+   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   <title>about</title>
+
+   <!-- swiper css link  -->
+   <link rel="stylesheet" href="https://unpkg.com/swiper@7/swiper-bundle.min.css" />
+
+   <!-- font awesome cdn link  -->
+   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
+
+   <!-- custom css file link  -->
+   <link rel="stylesheet" href="css/style.css">
+
+</head>
+
+<body>
+
+   <!-- header section starts  -->
+
+   <section class="header">
+
+      <a href="home.php" class="logo">Shoes store</a>
+
+      <nav class="navbar">
+         <a href="home.php">home</a>
+         <a href="about.php">about</a>
+         <a href="package.php"> brand</a>
+         <a href="book.php">book</a>
+         <a href="sign/login.php">Signin/Signup</a>
+      </nav>
+
+      <div id="menu-btn" class="fas fa-bars"></div>
+
+   </section>
+
+   <!-- header section ends -->
+
+   <div class="heading" style="background:url(images/home.jpg) no-repeat">
+      <h1>about us</h1>
+   </div>
+
+   <!-- about section starts  -->
+
+   <section class="about">
+
+      <div class="image">
+         <img src="images/about-img.jpg" alt="">
+      </div>
+
+      <div class="content">
+         <h3>why choose us?</h3>
+         <p>Shopping for shoes online can be a convenient and cost-effective way to find the perfect pair. Many online shoe stores offer a wide selection of styles and brands, making it easy to find the right fit for any occasion. Plus, online retailers often have sales and discounts that can save you money.</p>
+         <div class="icons-container">
+            <div class="icons">
+               <i class="fas fa-map"></i>
+               <span>top Brands</span>
+            </div>
+            <div class="icons">
+               <i class="fas fa-hand-holding-usd"></i>
+               <span>affordable price</span>
+            </div>
+            <div class="icons">
+               <i class="fas fa-headset"></i>
+               <span>24/7 available</span>
+            </div>
+         </div>
+      </div>
+
+   </section>
+
+   <!-- about section ends -->
+
+   <!-- reviews section starts  -->
+
+   <section class="reviews">
+
+      <h1 class="heading-title"> clients reviews </h1>
+
+      <div class="swiper reviews-slider">
+
+         <div class="swiper-wrapper">
+
+            <div class="swiper-slide slide">
+               <div class="stars">
+                  <i class="fas fa-star"></i>
+                  <i class="fas fa-star"></i>
+                  <i class="fas fa-star"></i>
+                  <i class="fas fa-star"></i>
+                  <i class="fas fa-star"></i>
+               </div>
+               <p>I am in love with these shoes! They fit perfectly and are so comfortable. I've worn them all day and my feet didn't hurt at all. Highly recommend!</p>
+               <h3>Alin Ale</h3>
+               <span>Student</span>
+               <img src="images/pic-1.png" alt="">
+            </div>
+
+            <div class="swiper-slide slide">
+               <div class="stars">
+                  <i class="fas fa-star"></i>
+                  <i class="fas fa-star"></i>
+                  <i class="fas fa-star"></i>
+                  <i class="fas fa-star"></i>
+               </div>
+               <p>I was a little hesitant to buy shoes online, but these were a great purchase. The fit is true to size and the quality is impressive. I've worn them to work and on the weekends and they look and feel great.p>
+               <h3>Aaruhi Sen</h3>
+               <span>Vlogger</span>
+               <img src="images/pic-2.png" alt="">
+            </div>
+
+            <div class="swiper-slide slide">
+               <div class="stars">
+                  <i class="fas fa-star"></i>
+                  <i class="fas fa-star"></i>
+               </div>
+               <p>I was disappointed with these shoes. They were too tight and caused blisters on my feet. The quality also seemed lacking, as they started to fall apart after only a few wears</p>
+               <h3>Aaditya Pokhrel</h3>
+               <span>Businessman</span>
+               <img src="images/pic-3.png" alt="">
+            </div>
+
+            <div class="swiper-slide slide">
+               <div class="stars">
+                  <i class="fas fa-star"></i>
+                  <i class="fas fa-star"></i>
+                  <i class="fas fa-star"></i>
+                  <i class="fas fa-star"></i>
+                  <i class="fas fa-star"></i>
+               </div>
+               <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Possimus recusandae cum quibusdam odio
+                  dignissimos nemo?</p>
+               <h3>john deo</h3>
+               <span>traveler</span>
+               <img src="images/pic-4.png" alt="">
+            </div>
+
+            <div class="swiper-slide slide">
+               <div class="stars">
+                  <i class="fas fa-star"></i>
+                  <i class="fas fa-star"></i>
+                  <i class="fas fa-star"></i>
+               </div>
+               <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Minus facilis laudantium magnam saepe magni
+                  ullam possimus laborum voluptas, veniam ipsum officiis quae doloribus minima ut quis. Molestiae qui
+                  distinctio possimus?</p>
+               <h3>john deo</h3>
+               <span>traveler</span>
+               <img src="images/pic-5.png" alt="">
+            </div>
+
+            <div class="swiper-slide slide">
+               <div class="stars">
+                  <i class="fas fa-star"></i>
+                  <i class="fas fa-star"></i>
+                  <i class="fas fa-star"></i>
+                  <i class="fas fa-star"></i>
+               </div>
+               <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Minus facilis laudantium magnam saepe magni
+                  ullam possimus laborum voluptas, veniam ipsum officiis quae doloribus minima ut quis. Molestiae qui
+                  distinctio possimus?</p>
+               <h3>john deo</h3>
+               <span>traveler</span>
+               <img src="images/pic-6.png" alt="">
+            </div>
+
+         </div>
+
+      </div>
+
+   </section>
+
+   <!-- reviews section ends -->
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+   <!-- footer section starts  -->
+
+   <section class="footer">
+
+      <div class="box-container">
+
+         <div class="box">
+            <h3>quick links</h3>
+            <a href="home.php"> <i class="fas fa-angle-right"></i> home</a>
+            <a href="about.php"> <i class="fas fa-angle-right"></i> about</a>
+            <a href="package.php"> <i class="fas fa-angle-right"></i> Brand</a>
+            <a href="book.php"> <i class="fas fa-angle-right"></i> book</a>
+         </div>
+
+         <!-- <div class="box">
+            <h3>extra links</h3>
+            <a href="#"> <i class="fas fa-angle-right"></i> ask questions</a>
+            <a href="#"> <i class="fas fa-angle-right"></i> about us</a>
+            <a href="#"> <i class="fas fa-angle-right"></i> privacy policy</a>
+            <a href="#"> <i class="fas fa-angle-right"></i> terms of use</a>
+         </div> -->
+
+         <div class="box">
+            <h3>contact info</h3>
+            <a href="#"> <i class="fas fa-phone"></i> 9803216547</a>
+            <a href="#"> <i class="fas fa-phone"></i> 9874563210 </a>
+            <a href="#"> <i class="fas fa-envelope"></i> bhola@.com </a>
+            <a href="#"> <i class="fas fa-map"></i>Kathmandu, Nepal </a>
+         </div>
+
+         <div class="box">
+            <h3>follow us</h3>
+            <a href="https://www.facebook.com/profile.php?id=100080251969771" target="_blank"> <i class="fab fa-facebook-f"></i> facebook </a>
+            <a href="#"> <i class="fab fa-twitter"></i> twitter </a>
+            <a href="https://www.instagram.com/bholarauniyar13/" target="_blank"> <i class="fab fa-instagram"></i> instagram </a>
+            <a href="#"> <i class="fab fa-linkedin"></i> linkedin </a>
+         </div>
+
+      </div>
+
+   </section>
+
+   <!-- footer section ends -->
+
+
+
+
+
+
+
+
+
+   <!-- swiper js link  -->
+   <script src="https://unpkg.com/swiper@7/swiper-bundle.min.js"></script>
+
+   <!-- custom js file link  -->
+   <script src="js/script.js"></script>
+
+</body>
+
+</html>
